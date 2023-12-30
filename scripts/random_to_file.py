2f72e0aca185b36f887a4cd739c1e363d5f6360e
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-12-30 11:12:16 +1100

    fix

--- scripts/random_to_file.py
+++ scripts/random_to_file.py
@@ -1,10 +1,18 @@
+import argparse
 import random
 
 def main():
+    args = parse_args()
+    output_path = args.output_path
     random_number = random.randint(1, 100)
-    with open("random_number.txt", "w") as f:
+    with open(output_path, "w") as f:
         f.write(str(random_number))
-    print("Random number generated and saved to file.")
+    print(f"Random number generated and saved to file: {output_path}")
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output-path", type=str, default="random_number.txt")
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
