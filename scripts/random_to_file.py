c9f99b88fe1953de06775a0fc4b7a746d9f8eb0b
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-12-30 11:00:14 +1100

    gitignore + structure

--- scripts/random_to_file.py
+++ scripts/random_to_file.py
@@ -1,6 +1,10 @@
 import random
 
-if __name__ == "__main__":
+def main():
     random_number = random.randint(1, 100)
     with open("random_number.txt", "w") as f:
         f.write(str(random_number))
+    print("Random number generated and saved to file.")
+
+if __name__ == "__main__":
+    main()
